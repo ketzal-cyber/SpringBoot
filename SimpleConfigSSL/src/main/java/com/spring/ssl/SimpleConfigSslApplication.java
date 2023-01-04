071d2b869fe3c42624e11bc2408a54d14fdc98ec
ketzal-cyber <hernandezmoreno.angel10@gmail.com>
2023-01-03 23:18:39 -0600

    config ssl properties

---
+++ SimpleConfigSSL/src/main/java/com/spring/ssl/SimpleConfigSslApplication.java
@@ -0,0 +1,13 @@
+package com.spring.ssl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleConfigSslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleConfigSslApplication.class, args);
+	}
+
+}
